9410d16a7e4581965e818c7e82f6fcca6769943e
Hector9473 <hectorhsalazarf@gmail.com>
2026-09-24 12:26:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ MODO_EDICION = False
 FOTOS_FIJAS = {
     "portada": "1.jpg",      # fondo de la portada
     "historia": "2.jpg, 3.jpg",     # foto en arco sobre "Una historia de amor"
-    "galeria": [3.jpg , 2.jpg],        # lista de fotos, en el orden en que se mostrarán
+    "galeria": ["3.jpg , 2.jpg"],        # lista de fotos, en el orden en que se mostrarán
     "vestuario": None,    # imagen de referencia del código de vestuario
     "cierre": "8.jpg",       # fondo de la sección final
 }
