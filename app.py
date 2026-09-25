52a6ab3051c311356c8fcdd0ff745bc9fe5b439f
Hector9473 <hectorhsalazarf@gmail.com>
2026-09-25 13:53:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ MODO_EDICION = True
 #    ya armado en la sección "3. Dejarlo fijo".
 FOTOS_FIJAS = {
     "portada": "1.jpg",      # fondo de la portada
-    "historia": ["2.jpg, 3.jpg, 4.jpg, 5.jpg"],       # 1 foto = arco; 2 a 4 fotos = collage
+    "historia": ["2.jpg", "3.jpg", "4.jpg", "5.jpg"],       # 1 foto = arco; 2 a 4 fotos = collage
     "galeria": [],        # lista de fotos, en el orden en que se mostrarán
     "vestuario": "9.jpge",    # imagen de referencia del código de vestuario
     "cierre": "8.jpg",       # fondo de la sección final
